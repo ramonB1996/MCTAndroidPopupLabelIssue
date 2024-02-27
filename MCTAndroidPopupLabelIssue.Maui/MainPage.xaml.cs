e96280214c36182019a4b9a087db04144f4f3f21
RJ Abacherli <rj.abacherli@bdmcc.net>
2024-02-27 12:54:50 +0100

    Init

---
+++ MCTAndroidPopupLabelIssue.Maui/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Maui.Views;
+using StackLayout = Microsoft.Maui.Controls.Compatibility.StackLayout;
+
+namespace MCTAndroidPopupLabelIssue.Maui;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_OnClicked(object? sender, EventArgs e)
+    {
+        this.ShowPopup(new Popup());
+    }
+}
---
+++ MCTAndroidPopupLabelIssue.Maui/Popup.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCTAndroidPopupLabelIssue.Maui;
+
+public partial class Popup : CommunityToolkit.Maui.Views.Popup
+{
+    public Popup()
+    {
+        InitializeComponent();
+    }
+}
